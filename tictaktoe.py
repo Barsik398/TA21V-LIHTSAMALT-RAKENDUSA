62c4ec772d4a41d9595f3ad2c08cda6b9510a8bc
BARSIK397 <94055301+BARSIK397@users.noreply.github.com>
2022-03-24 11:26:41 +0200

    game in 50000 line

--- tictaktoe.py
+++ tictaktoe.py
@@ -4,7 +4,6 @@ if next_position == "1":
     print("X . .")
     print(". . .")
     print(". . .")
-
 if next_position == "2":
     print(". X .")
     print(". . .")
@@ -37,3 +36,78 @@ if next_position == "9":
     print(". . .")
     print(". . .")
     print(". . X")
+
+    next_position = int(input("please type what position"))
+
+    if next_position = "1":
+        print("CANT DO MOVE")
+    elif next_position == "2":
+        print("X o .")
+        print(". . .")
+        print(". . .")
+    elif next_position == "3":
+        print("X . o")
+        print(". . .")
+        print(". . .")
+    elif next_position == "4":
+        print("X . .")
+        print("o . .")
+        print(". . .")
+    elif next_position == "5":
+        print("X . .")
+        print(". o .")
+        print(". . .")
+    elif next_position == "6":
+        print("X . .")
+        print(". . o")
+        print(". . .")
+    elif next_position == "7":
+        print("X . .")
+        print(". . .")
+        print("o . .")
+    elif next_position == "8":
+        print("X . .")
+        print(". . .")
+        print(". o .")
+    elif next_position == "9":
+        print("X . .")
+        print(". . .")
+        print(". . o")
+        
+        next_position = int(input("please type what position"))
+        if next_position = "2":
+            print("CANT DO MOVE")
+        elif next_position == "1":
+            print("o X .")
+            print(". . .")
+            print(". . .")
+        elif next_position == "3":
+            print(". X o")
+            print(". . .")
+            print(". . .")
+        elif next_position == "4":
+            print(". X .")
+            print("o . .")
+            print(". . .")
+        elif next_position == "5":
+            print(". X .")
+            print(". o .")
+            print(". . .")
+        elif next_position == "6":
+            print(". X .")
+            print(". . o")
+            print(". . .")
+        elif next_position == "7":
+            print(". X .")
+            print(". . .")
+            print("o . .")
+        elif next_position == "8":
+            print(". X .")
+            print(". . .")
+            print(". o .")
+        elif next_position == "9":
+            print(". X .")
+            print(". . .")
+            print(". . o")
+
+
